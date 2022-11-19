42a95390194492c39635a7ec8c82534b7cfff9e8
chrris99 <conforti.christian@gmail.com>
2022-11-19 17:36:12 +0100

    add marker interfaces for auditable and soft-deletable entities

---
+++ src/Fort.Core/Abstractions/ISoftDeletableEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Fort.Core.Abstractions;
+
+/// <summary>
+/// Marker interface to represent soft-deletable entities.
+/// </summary>
+public interface ISoftDeletableEntity
+{
+    /// <summary>
+    /// Gets the deleted on date and time in UTC format.
+    /// </summary>
+    DateTime? DeletedOnUtc { get; }
+    
+    /// <summary>
+    /// Gets the deleted flag value, indicating whether the entity has been deleted.
+    /// </summary>
+    bool Deleted { get; }
+}
